2636d9471a32ab59d3a01b8807304016ad8670b0
smaicas <samuel.maicas.development@gmail.com>
2022-12-10 15:30:54 +0100

    Add Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Solution
+
+using Day3;
+Console.WriteLine("Advent of Code 2022 - Day 1");
+Console.WriteLine(Environment.NewLine);
+Solution resolver = new();
+Console.WriteLine($"Part 1: {resolver.ResolvePart1()}");
+Console.WriteLine($"Part 2: {resolver.ResolvePart2()}");
+Console.ReadKey();
+
+// Benchmark
+
+//using BenchmarkDotNet.Running;
+//using Day3;
+//BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<Solution>();
+//Console.ForegroundColor = ConsoleColor.DarkCyan;
+//Console.WriteLine("Benchmark ended!. Press any key to close");
+//Console.ResetColor();
+//Console.ReadKey();
+
+//BenchmarkDotNet=v0.13.2, OS=Windows 11 (10.0.22000.1219/21H2)
+//AMD Ryzen 9 3900X, 1 CPU, 24 logical and 12 physical cores
+//    .NET SDK=7.0.100
+//    [Host]     : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
+//DefaultJob : .NET 7.0.0 (7.0.22.51805), X64 RyuJIT AVX2
+
+//    |       Method |       Mean |    Error |   StdDev | Rank |     Gen0 |    Gen1 | Allocated |
+//    |------------- |-----------:|---------:|---------:|-----:|---------:|--------:|----------:|
+//    | ResolvePart1 |   925.5 us | 11.74 us |  9.16 us |    1 | 125.0000 | 14.6484 |      1 MB |
+//    | ResolvePart2 | 1,055.5 us | 14.70 us | 20.13 us |    2 | 136.7188 | 17.5781 |    1.1 MB |
---
+++ Day3/Solution.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Day3;
+
+[RankColumn]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class Solution
+{
+    /// <exception cref="RegexMatchTimeoutException">A time-out occurred. For more information about time-outs, see the Remarks section.</exception>
+    /// <exception cref="ArgumentException">A regular expression parsing error occurred.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="input" /> or <paramref name="pattern" /> is <see langword="null" />.</exception>
+    /// <exception cref="EncoderFallbackException">A fallback occurred (for more information, see Character Encoding in .NET)
+    ///  -and-
+    ///  <see cref="EncoderFallback" /> is set to <see cref="EncoderExceptionFallback" />.</exception>
+    /// <exception cref="OutOfMemoryException">There is insufficient memory to allocate a buffer for the returned string.</exception>
+    /// <exception cref="IOException">An I/O error occurs.</exception>
+    /// <exception cref="FileNotFoundException">The file cannot be found.</exception>
+    /// <exception cref="DirectoryNotFoundException">The specified path is invalid, such as being on an unmapped drive.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="startIndex" /> plus <paramref name="length" /> indicates a position not within this instance.
+    ///  -or-
+    ///  <paramref name="startIndex" /> or <paramref name="length" /> is less than zero.</exception>
+    /// <exception cref="IndexOutOfRangeException"><paramref name="index" /> is greater than or equal to the length of this object or less than zero.</exception>
+    [Benchmark]
+    public int ResolvePart1()
+    {
+        const string data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        IEnumerable<string> lines = ReadFileLines("input.txt");
+        int total = 0;
+        foreach (string line in lines)
+        {
+            int half = line.Length / 2;
+            string part1 = line[..half];
+            string part2 = line.Substring(half, half);
+            string regex =
+                @$"([{part1}])";
+            Match match = Regex.Match(part2, regex);
+            total += data.IndexOf(match.Value[0].ToString(), StringComparison.Ordinal) + 1;
+        }
+        return total;
+
+        static IEnumerable<string> ReadFileLines(string filePath)
+        {
+            using StreamReader reader = new(filePath);
+
+            while (reader.ReadLine() is { } line)
+            {
+                yield return line;
+            }
+        }
+    }
+
+    /// <exception cref="ArgumentOutOfRangeException">Enlarging the value of this instance would exceed <see cref="P:System.Text.StringBuilder.MaxCapacity" />.</exception>
+    /// <exception cref="OutOfMemoryException">There is insufficient memory to allocate a buffer for the returned string.</exception>
+    /// <exception cref="IOException">An I/O error occurs.</exception>
+    /// <exception cref="RegexMatchTimeoutException">A time-out occurred. For more information about time-outs, see the Remarks section.</exception>
+    /// <exception cref="ArgumentException">A regular expression parsing error occurred.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="values" /> is <see langword="null" />.</exception>
+    /// <exception cref="IndexOutOfRangeException"><paramref name="index" /> is greater than or equal to the length of this object or less than zero.</exception>
+    [Benchmark]
+    public int ResolvePart2()
+    {
+        const string data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        IEnumerable<string> lines = ReadFileLines("input.txt");
+        int total = 0;
+        foreach (string line in lines)
+        {
+            string[] split = line.Split(',');
+            string regex =
+                @$"([{split[0]}])";
+            MatchCollection match = Regex.Matches(split[1], regex);
+            regex = @$"([{string.Join("", match)}])";
+            Match match2 = Regex.Match(split[2], regex);
+            total += data.IndexOf(match2.Value[0].ToString(), StringComparison.Ordinal) + 1;
+        }
+        return total;
+
+        static IEnumerable<string> ReadFileLines(string filePath)
+        {
+            using StreamReader reader = new(filePath);
+
+            while (reader.ReadLine() is { } line)
+            {
+                StringBuilder sb = new();
+                sb.Append(line);
+                sb.Append(",");
+                sb.Append(reader.ReadLine());
+                sb.Append(",");
+                sb.Append(reader.ReadLine());
+
+                yield return sb.ToString();
+            }
+        }
+    }
+}
